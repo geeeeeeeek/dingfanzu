dd6dcde60710c9acd094ce6595453eba9544cbe9
chanson1024 <lovezww2011@gmail.com>
2017-03-25 22:41:15 +0800

    Added a DEBUG

--- configs/configs.php
+++ configs/configs.php
@@ -1,9 +1,11 @@
 <?php 
 define("DB_HOST","localhost");
-define("DB_USER","");
+define("DB_USER","root");
 define("DB_PWD","");
 define("DB_DBNAME","dfz");
 define("DB_CHARSET","utf8");
+
+define("DEBUG",true);
 
 define("IMG_50","http://".DB_HOST."/image_50/");
 
